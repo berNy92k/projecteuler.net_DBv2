691d5b877212db386514c05ab07c9d3f1be05f58
Dawid Bernacki <daw.bernacki@gmail.com>
2020-08-18 00:35:48 +0200

    Main change: problem 2

---
+++ src/projectEuler/till10/Problem10.java
@@ -0,0 +1,15 @@
+package projectEuler.till10;
+
+public class Problem10 {
+
+    public static void main(String[] args) {
+        int result = method();
+
+        System.out.println(result);
+    }
+
+    private static int method () {
+
+        return 0;
+    }
+}
